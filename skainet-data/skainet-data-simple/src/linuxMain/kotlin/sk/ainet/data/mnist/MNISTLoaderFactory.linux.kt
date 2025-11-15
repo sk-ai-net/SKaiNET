7eaf1f6dd2c51671b267c68acb9ffe238dd91165
Michal Harakal <michal.harakal@googlemail.com>
2025-11-15 17:54:01 +0100

    Add initial support for model code sharing API (model definition, execution, loading)
    Implements: #196
    Related-To: #169

---
+++ skainet-data/skainet-data-simple/src/linuxMain/kotlin/sk/ainet/data/mnist/MNISTLoaderFactory.linux.kt
@@ -0,0 +1,15 @@
+package sk.ainet.data.mnist
+
+public actual object MNISTLoaderFactory {
+    public actual fun create(): sk.ainet.data.mnist.MNISTLoader {
+        TODO("Not yet implemented")
+    }
+
+    public actual fun create(cacheDir: String): sk.ainet.data.mnist.MNISTLoader {
+        TODO("Not yet implemented")
+    }
+
+    public actual fun create(config: sk.ainet.data.mnist.MNISTLoaderConfig): sk.ainet.data.mnist.MNISTLoader {
+        TODO("Not yet implemented")
+    }
+}
