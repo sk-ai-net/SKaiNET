0e101eb5fc2d05d17bed799a0b9af04b498a7e52
Michal Harakal <michal.harakal@googlemail.com>
2025-12-02 22:01:03 +0100

    Fix macosx implementaion of image processing
    Implements: #223
    Related-To #220, #221

---
+++ skainet-io/skainet-io-image/src/linuxArm64Main/kotlin/sk/ainet/io/image/ImageInterop.linuxArm64.kt
@@ -0,0 +1,23 @@
+package sk.ainet.io.image
+
+import sk.ainet.context.ExecutionContext
+import sk.ainet.lang.tensor.Tensor
+import sk.ainet.lang.types.FP16
+
+actual class PlatformBitmapImage
+
+actual fun platformImageToArgb(image: PlatformBitmapImage, ctx: ExecutionContext): Tensor<FP16, Float> {
+    throw NotImplementedError("platformImageToArgb is not implemented for Linux arm64 yet")
+}
+
+actual fun argbToPlatformImage(image: Tensor<FP16, Float>, ctx: ExecutionContext): PlatformBitmapImage {
+    throw NotImplementedError("argbToPlatformImage is not implemented for Linux arm64 yet")
+}
+
+actual fun platformImageToRgbByteArray(image: PlatformBitmapImage): ByteArray {
+    throw NotImplementedError("platformImageToRgbByteArray is not implemented for Linux arm64 yet")
+}
+
+actual fun platformImageSize(image: PlatformBitmapImage): Pair<Int, Int> {
+    throw NotImplementedError("platformImageSize is not implemented for Linux arm64 yet")
+}
